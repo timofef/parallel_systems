10683d3858f49c7193dfef7cd9a13cb6740e5141
Timofei Makarov <macintosh1999@mail.ru>
2023-03-13 16:38:35 +0300

    lab1 works

---
+++ lab1/main.py
@@ -0,0 +1,71 @@
+import numpy as np
+import numpy.linalg as lin
+
+
+def get_incidence_matrix():
+    f = open("matrix.csv")
+
+    dim = int(f.readline())
+    m = np.zeros((dim, dim))
+
+    for line in f.readlines():
+        split = line.split(' ')
+        a = int(split[0]) - 1
+        b = int(split[1]) - 1
+        m[a][b] = 1
+        m[b][a] = 1
+
+    f.close()
+
+    return m
+
+
+def get_vertices_degrees_matrix(incidence_matrix: np.array):
+    m = np.zeros(len(incidence_matrix))
+    for i in range(len(incidence_matrix)):
+        m[i] = np.sum(incidence_matrix[i])
+
+    return np.diag(m)
+
+
+def get_eigen(laplace: np.array):
+    values, vectors = lin.eigh(laplace)
+    indexes = np.argsort(values)
+
+    values = values[indexes]
+    vectors = vectors[indexes]
+
+    vectors = np.array([vectors[i]/lin.norm(vectors[i]) for i in range(len(vectors))])
+
+    return values, vectors
+
+
+def main():
+    A = get_incidence_matrix()
+    B = get_vertices_degrees_matrix(A)
+    L = B - A
+
+    print(A)
+    print(B)
+    print(L)
+
+    eigen_values, eigen_vectors = get_eigen(L)
+
+    a = []
+    b = []
+    t = np.array([eigen_vectors[i][1] for i in range(len(eigen_vectors))])
+    avg = np.average(t)
+    print(eigen_values)
+    print(eigen_vectors)
+
+    for i in range(len(t)):
+        if t[i] < avg:
+            a.append(i + 1)
+        if t[i] > avg:
+            b.append(i + 1)
+    print(a)
+    print(b)
+
+
+if __name__ == '__main__':
+    main()
